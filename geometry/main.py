0fe1d6d8672151b9c3a6de80654299d1f46a9a3b
Anna <Livonchykanna2009@gmail.com>
2025-12-20 16:11:08 +0200

    first commit

---
+++ geometry/main.py
@@ -0,0 +1,81 @@
+import os
+from telegram import  Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+from model import get_solution
+
+TOKEN = "8430042770:AAE_ZpG-QT_ltHD75r33Zh-WQG1i2aaSEws"
+
+
+
+
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    subjects = """Г - Геометрія
+А - Алгебра
+Х - Хімія"""
+    await update.message.reply_html(
+        "Привіт!Це бот який допомагає вирішувати домашні завдання.Для того щоб скинути завдання відправте скріншот завдання з підписом - перша літера назву предмету(велика).",
+    )
+    await update.message.reply_html(
+        subjects
+    )
+    
+
+
+
+async def solution(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Shows the help message when the command /help is issued."""
+    user_choice = update.message.caption
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("ex.jpg")
+    if user_choice == "Г":
+        text = get_solution("Геометрія")
+        os.remove("ex.jpg")
+        await update.message.reply_text(
+            text= text,
+        )
+    elif user_choice == "А":
+        text = get_solution("Алгебра")
+        os.remove("ex.jpg")
+        await update.message.reply_text(
+            text= text,
+        )
+    elif user_choice == "Х":
+        text = get_solution("Хімія")
+        os.remove("ex.jpg")
+        await update.message.reply_text(
+            text= text,
+        )
+    elif user_choice == "Ф":
+        text = get_solution("Фізика")
+        os.remove("ex.jpg")
+        await update.message.reply_text(
+            text= text,
+        )
+
+def main() -> None:
+    """Start the bot."""
+
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+
+    
+    
+    application.add_handler(MessageHandler(filters.PHOTO & filters.CAPTION, solution))
+    
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
---
+++ geometry/model.py
@@ -0,0 +1,38 @@
+from google import genai
+from google.genai import types
+
+
+def get_solution(sub) -> str:
+    try:
+        with open("ex.png","rb") as r_ex:
+            result = r_ex.read()
+            mt = "image/png"
+    except:
+        try:
+            with open("ex.jpg","rb") as r_ex:
+                result = r_ex.read()
+                mt = "image/jpeg"
+        except:
+            with open("ex.jpeg","rb") as r_ex:
+                result = r_ex.read()
+                mt = "image/jpeg"
+    if sub == "Геометрія":
+        user_request = "Виріши мені задачу з геометрії. Намалюй мені схематично фігуру текстовими символами.Оформи рішення стисло,покроково з такими пунктами: Дано,знайти,рішення. "
+    elif sub == "Алгебра":
+        user_request = "Виріши мені задачу з алгебри. Якщо задача потребує намалюй мені схематично текстовими символами. Оформи рішення максимально стисло та покроково. "
+    elif sub == "Хімія":
+        user_request = "Виріши мені задачу з хімії. Оформи рішення максимально стисло ."
+    elif sub == "Фізика":
+        user_request = "Виріши мені задачу з фізики. Оформи рішення максимально стисло за правилами оформлення з фізики, якщо це потрібно."
+    client = genai.Client(api_key="AIzaSyChrnM-PIxEF5Iw5Akm26QisPVBsB4lHiE")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", 
+        contents = [
+            user_request,
+            types.Part.from_bytes(
+                data=result,
+                mime_type=mt
+            )
+        ]
+    )
+    return response.text
